c2897edf12d00fc7febc4550b405d4080aa27527
Muhammad Imtiaz <imtiaz9832@gmail.com>
2023-05-31 13:10:45 +0500

    Update drop-dbs.sh

--- drop-dbs.sh
+++ drop-dbs.sh
@@ -1,11 +1,13 @@
-databases=("airbyte" "asset_service" "auth_service" "cadence" "cadence_visibility" "auth_service_keycloak" "data_analysis_service" "data_product_service" "dbt_transformations_service" "ml_service" "pipeline_service" "product_service" "sql_transformations_service" "streaming_source_connector_service" "superset_service")
+DATABASES=("airbyte" "asset_service" "auth_service" "cadence" "cadence_visibility" "auth_service_keycloak" "data_analysis_service" "data_product_service" "dbt_transformations_service" "ml_service" "pipeline_service" "product_service" "sql_transformations_service" "streaming_source_connector_service" "superset_service")
 
+# Update the credentials
+export PGUSER='<db-user>';
 export PGPASSWORD='<password>';
 export PGHOST='<db-host>';
-export USER='<db-user>';
-for db in ${databases[@]};
+
+for db in ${DATABASES[@]};
 do
   echo "Deleting database $db"
   psql -U $USER -h $PGHOST -c "drop database $db WITH (FORCE)";
 done
-echo All done
+echo "Cheers! All done :)!"
